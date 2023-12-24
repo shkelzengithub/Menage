ab85930e83cd978525be5bb57b0f1384c997025d
shkelzen muja <shkelzenmuja@shkelzens-MacBook-Air.local>
2023-12-24 16:35:22 +0100

    Project Advanced Database

---
+++ server/sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+      id: '1',
+      clientId: '1',
+      name: 'eCommerce Website',
+      description:
+        'Ecommerce, short for electronic commerce, involves buying and selling goods and services online. Transactions take place via websites, mobile apps, and online marketplaces like Amazon—not in a physical store. ',
+      status: 'In Progress',
+    },
+    {
+      id: '2',
+      clientId: '2',
+      name: 'Dating App',
+      description:
+        'An online dating application is an online dating service presented through a mobile phone application (app). These apps often take advantage of a smartphones GPS location capabilities, always on-hand presence, and access to mobile wallets. ',
+      status: 'In Progress',
+    },
+    {
+      id: '3',
+      clientId: '3',
+      name: 'SEO Project',
+      description:
+        'SEOWRITEE is an AI writing tool for copywriters who want to create quality content. Get your SEO article optimized for search engines as Google just now! SEOWRITEE will provide you with the best content ideas and AI-written text  based on chosen keyword. ChatGPT included.',
+      status: 'In Progress',
+    },
+    {
+      id: '4',
+      clientId: '4',
+      name: 'Design Prototype',
+      description:
+        'Bring your idea to life in no time. The prototyping solution for all your needs. For UX designers, entrepreneurs, product managers, marketers, and anyone with a great idea.',
+      status: 'Done',
+    },
+    {
+      id: '5',
+      clientId: '5',
+      name: 'Auction Website',
+      description:
+        'Sell From Your Site — Helping businesses to buy and sell commercial assets in the smartest way possible.',
+      status: 'In Progress',
+    },
+  ];
+  
+  // Clients
+  const clients = [
+    {
+      id: '1',
+      name: 'Shani Grazhda',
+      email: 'shgrazhda@gmail.com',
+      phone: '044-567-433',
+    },
+    {
+      id: '2',
+      name: 'Amina Rahmani',
+      email: 'arahmani@gmail.com',
+      phone: '044-567-332',
+    },
+    {
+      id: '3',
+      name: 'Sami Shamli',
+      email: 'shamli@gmail.com',
+      phone: '045-331-333',
+    },
+    {
+      id: '4',
+      name: 'Kemal Karahoda',
+      email: 'kkemal@gmail.com',
+      phone: '044-562-678',
+    },
+    {
+      id: '5',
+      name: 'Bujar Pasha',
+      email: 'bpasha@gmail.com',
+      phone: '044-468-888',
+    },
+  ];
+  
+  module.exports = { projects, clients };
